1c00de1890e0d262115a28d0f2bfbe993aad8b34
Leal <douglas.sleal@outlook.com>
2024-09-03 23:40:10 -0300

    test

--- jarvis/main.py
+++ jarvis/main.py
@@ -1,18 +1,34 @@
-import os
-import importlib
+# import os
+# import importlib
 
 
-def run_actions():
-    # Listar todos os arquivos na pasta
+# def run_actions():
+#     # Listar todos os arquivos na pasta
 
-    action = 'ingest'
+#     action = 'ingest'
 
-    if action == 'ingest':
-        # Importar o módulo de ingestão
-        from actions.ingest import run
+#     if action == 'ingest':
+#         # Importar o módulo de ingestão
+#         from actions.ingest import run
 
-        run.exec()
+#         run.exec()
 
 
-if __name__ == '__main__':
-    run_actions()
+# if __name__ == '__main__':
+#     run_actions()
+
+# script.py
+import yaml
+import sys
+
+def main(config_path):
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+        print("Config settings:", config)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <config_path>")
+        sys.exit(1)
+    config_path = sys.argv[1]
+    main(config_path)
