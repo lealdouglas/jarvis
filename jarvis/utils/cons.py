1f092a2548a8c30344bb76eb849748c6f80bcbf5
Leal <douglas.sleal@outlook.com>
2024-09-03 22:53:53 -0300

    secrets

--- jarvis/utils/cons.py
+++ jarvis/utils/cons.py
@@ -1,11 +1,11 @@
 import os
 
 # Configuracoes do service principal1
-CLIENT_ID = ''
-TENANT_ID = ''
-CLIENT_SECRET = ''
+CLIENT_ID = os.environ.get('ARM_CLIENT_ID', 'Not Set')
+TENANT_ID = os.environ.get('ARM_TENANT_ID', 'Not Set')
+CLIENT_SECRET = os.environ.get('ARM_CLIENT_SECRET', 'Not Set')
 
 # Configuracoes do ambiente azure
-SUBSCRIPTION_ID = ''
+SUBSCRIPTION_ID = os.environ.get('ARM_SUBSCRIPTION_ID', 'Not Set')
 
 # RESOURCE_GROUP_NAME = (os.environ.get('RESOURCE_GROUP_NAME', 'RESOURCE_GROUP_NAME'))
